478aa260b1a3a2b597dc4d2ab0aaa5a3e0db27df
Robin Doirin <robindoirin@gmail.com>
2018-07-11 00:54:47 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -11,4 +11,8 @@ class Song < ActiveRecord::Base
   def genre_name
     self.genre ? self.genre.name : nil
   end
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+  end
 end
