7a3ae6a14d6e27590287c5834a2fa3341db31021
Robin Doirin <robindoirin@gmail.com>
2018-07-11 00:57:40 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -15,4 +15,20 @@ class Song < ActiveRecord::Base
   def artist_name=(name)
     self.artist = Artist.find_or_create_by(name: name)
   end
+
+  def artist_name(name)
+    self.artist ? self.artist.name : nil
+  end
+
+  def note_contents=(contents)
+    contents.each do |content|
+      if content.strip != ""
+        self.notes.build(content: content)
+      end
+    end
+  end
+
+  def note_contents
+    self.note.map(&:content)
+  end
 end
